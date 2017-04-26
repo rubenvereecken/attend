b157372eaf78581126b4a29dbc6ead61d3443025
Ruben Vereecken <rubenvereecken@gmail.com>
2017-04-26 12:35:51 +0100

    Basic scaffolding for big experiment

---
+++ run.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import time
+
+import simplejson as json
+
+parser = argparse.ArgumentParser(description='Run tests')
+# parser.add_argument('modality', type=str, choices=['A', 'V', 'AV'],
+#         default='AV')
+# parser.add_argument('annotation', type=str, choices='valence', 'arousal')
+parser.add_argument('-i', '--instances')
+# Have the annotations separate so we can switch other ones in
+parser.add_argument('-a', '--annotations')
+parser.add_argument('--log_dir', type=str, default='logs',
+                    help='Directory to hold logs')
+parser.add_argument('--no_gen_log_dir', dest='gen_log_dir',
+                    action='store_false',
+                    help="Don't generate timestamped log dir inside `log_dir`" + \
+                         " (default True)")
+
+parser.set_defaults(
+        gen_log_dir=True
+        )
+
+
+# Push instances through rescaling pipeline
+# Load annotations
+# Possibly separate script to convert annots from mat to hf5
+# Load pretrained network
+# Define keras training loop
+# Define train/test sets
+
+def _gen_log_dir(base_dir):
+    # Create a folder to hold results
+    time_str = time.strftime("%d-%m-%Y-%H-%M-%S", time.gmtime())
+    base_log_dir = time_str
+    log_dir = base_dir + '/' + base_log_dir
+    os.makedirs(log_dir, exist_ok=True)
+
+    link_path = base_dir + '/last'
+
+    try:
+        # Clean up old softlink
+        os.remove(link_path)
+    except OSError as ex:
+        pass # All G
+
+    os.symlink(base_log_dir, link_path)
+
+    return log_dir
+
+
+def _save_arguments(args, log_dir):
+    file_name = log_dir + '/params.json'
+    with open(file_name, 'w') as f:
+        json.dump(args.__dict__, f, sort_keys=True, indent='\t')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if args.gen_log_dir:
+        log_dir = _gen_log_dir(args.log_dir)
+    else:
+        log_dir = args.log_dir
+        os.makedirs(log_dir, exist_ok=True)
+
+    _save_arguments(args, log_dir)
+
