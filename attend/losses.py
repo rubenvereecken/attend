1ec9fa800a4a599a2363939365c862b2370acda7
Ruben Vereecken <rubenvereecken@gmail.com>
2017-09-04 12:31:52 +0100

    Implemented icc, batched, and streaming reduced mean

--- attend/losses.py
+++ attend/losses.py
@@ -12,3 +12,77 @@ def root_mean_squared_error(labels, predictions, weights=1., scope=None,
 
     return rmse
 
+
+def icc(case, typ,
+        labels, predictions, weights=1., scope=None,
+        loss_collection=tf.GraphKeys.LOSSES):
+    """
+    Functionally equivalent to Robert's
+
+    Assumes data of shape
+        B x n x r
+        where r is the number of ratings, usually 1
+        and n is the amount of targets, useful for sequences
+    """
+    labels.shape.assert_has_rank(3)
+
+    assert typ == 1, 'Only ICC(?,1) supported (type 1)'
+
+    # B x T x 1
+    y_hat = predictions
+    y_lab = labels
+
+    # B x 2 x T x 1
+    Y = tf.stack([y_hat, y_lab], axis=1)
+
+    # Number of ratings, should be 1 for my case
+    # k = Y.shape[3]
+    k = 2 # Robert seems to set this to 2 and it makes sense
+    # I am currently convinced k=2 because there is 1 truth and 1 prediction,
+    # per target, that is
+    k = Y.shape[1].value
+
+    # number of targets
+    n = Y.shape[2].value # I think this is T
+
+    # mean per target
+    mpt = tf.reduce_mean(Y, 1)  # B x T x 1
+
+    tm = tf.reduce_mean(mpt, 1) # B x 1
+
+    # mean per rating
+    mpr = tf.reduce_mean(Y, 2) # B x 2 x 1
+
+    # within target sum sqrs
+    WSS = tf.reduce_sum(
+        tf.square(Y[:,0] - mpt) +
+        tf.square(Y[:,1] - mpt), 1) # B x 1
+
+    # within mean sqrs
+    WMS = WSS / n / (k - 1) # B x 1
+
+    # Between rater sum sqrs
+    RSS = tf.reduce_sum(tf.square(mpr-tf.expand_dims(tm, 1)), 1) * n # B x 1
+
+    # Between rater mean sqrs
+    RMS = RSS / (k - 1) # B x 1
+
+    # Between target sum sqrs
+    BSS = tf.reduce_sum(tf.square(mpt-tf.expand_dims(tm, 1)), 1) * k # B x 1
+
+    # Between target mean squares
+    BMS = BSS / (n - 1) # B x 1
+
+    # Residual sum of squares
+    ESS = WSS - RSS # B x 1
+
+    # Residual mean squares
+    EMS = ESS / (n - 1) / (k - 1)
+
+    if case == 3:
+        if typ == 1:
+            res = (BMS - EMS) / (BMS + (k - 1) * EMS)
+
+    res # B x 1
+
+    return res
--- attend/metrics.py
+++ attend/metrics.py
@@ -25,3 +25,68 @@ def streaming_mse(predictions, targets, keys, lengths, mask=None):
 
     with tf.control_dependencies([total_update, count_update, mse_update]):
         return mse.lookup(keys), mse
+
+
+def streaming_reduce_mean(t, axis=None, weights=None, scope=None):
+    """
+    Axis can be an array of axes
+    """
+
+    if axis is None:
+        return tf.contrib.metrics.streaming_mean(t, weights)
+
+    if not type(axis) in [tuple, list]:
+        axis = [axis]
+
+
+    with tf.variable_scope(scope, 'streaming_reduce_mean'):
+        keep_axis = np.delete(np.arange(t.shape.ndims), axis)
+        dims = tf.shape(t)
+        out_shape = tf.gather(dims, keep_axis)
+        out_dims = np.delete(np.array(t.shape), axis) # tf.Dimension
+        reduce_dims = tf.gather(dims, axis)
+
+        total = tf.get_variable('total', trainable=False,
+                                initializer=tf.zeros(out_shape),
+                                collections=[tf.GraphKeys.LOCAL_VARIABLES],
+                                validate_shape=False)
+        count = tf.get_variable('count', trainable=False,
+                                initializer=0.,
+                                collections=[tf.GraphKeys.LOCAL_VARIABLES])
+        total.set_shape(out_dims)
+        reduced_count = tf.cast(tf.reduce_prod(reduce_dims), tf.float32)
+
+        total = tf.assign(total, total + tf.reduce_sum(t, axis))
+        count = tf.assign(count, count + reduced_count)
+
+        return total / count
+
+
+def streaming_icc(predictions, targets, lengths, mask=None,
+                  cse=3, typ=1):
+    """
+    'cse' is either 1,2,3. 'cse' is: 1 if each target is measured by a
+    different set of raters from a population of raters, 2 if each target is
+    measured by the same raters, but that these raters are sampled from a
+    population of raters, 3 if each target is measured by the same raters and
+    these raters are the only raters of interest.
+
+    'typ' is either 'single' or 'k' & denotes whether the ICC is based on a
+    single measurement or on an average of k measurements, where k = the
+    number of ratings/raters.
+
+    ONLY EXPECT THIS TO WORK FOR r,1! I.e single observation
+    """
+    # TODO rm Robert adds a leading 1-dimension, so I think it's batched
+    # B x T x 1
+    y_hat = predictions
+    y_lab = targets
+
+    Y = tf.stack([y_hat, y_lab])
+
+    # number of targets
+    n = Y.shape[2]
+
+    # mean per target
+    mpt = tf.contrib.metrics.streaming_mean()
+
